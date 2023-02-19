30fa866b3b90d478c8beabc5483d59f28e825351
orslow <jueonpk@gmail.com>
2023-02-20 00:41:02 +0900

    Add neovim configs

---
+++ nvim/lua/config/plugins-setup.lua
@@ -0,0 +1,82 @@
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
+end
+
+local packer_bootstrap = ensure_packer() -- true if packer was just installed
+
+-- autocommand $hat reloads neovim and installs/updates/removes plugins
+-- when file is saved
+vim.cmd([[ 
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins-setup.lua source <afile> | PackerSync
+  augroup end
+]])
+
+-- import packer safely
+local status, packer = pcall(require, 'packer')
+if not status then
+  return
+end
+
+return packer.startup(function(use)
+    use('wbthomason/packer.nvim')
+
+    use("nvim-lua/plenary.nvim")
+
+    use("christoomey/vim-system-copy")
+    -- use("hashivim/vim-terraform")
+
+    -- colorschemes
+    use("yasukotelin/shirotelin")
+
+    use("szw/vim-maximizer") -- maximizes and restores current window (tmux 'Z')
+
+    -- github.com/tpope/vim-surround/blob/master/README.markdown
+    use("tpope/vim-surround")
+
+    -- github.com/vim-scripts/ReplaceWithRegister/blob/master/README
+    use("vim-scripts/ReplaceWithRegister")
+
+    -- comment with gc
+    use("numToStr/Comment.nvim")
+
+    -- file explorer
+    use("nvim-tree/nvim-tree.lua")
+
+    -- nvim-tree icons
+    -- use("kyazdani42/nvim-web-devicons")
+
+    -- statusline
+
+    -- fuzzy finding
+    use({ "nvim-telescope/telescope-fzf-native.nvim", run = "make" })
+    use({ "nvim-telescope/telescope.nvim", branch = "0.1.x" })
+
+    -- autocompletion
+    use("hrsh7th/nvim-cmp")
+
+    -- managing & installing lsp servers, linters & formatters
+    use("williamboman/mason.nvim")
+    use("williamboman/mason-lspconfig.nvim")
+
+    -- configuring lsp servers
+    use("neovim/nvim-lspconfig")
+    use("hrsh7th/cmp-nvim-lsp")
+    use({ "glepnir/lspsaga.nvim", branch = "main" })
+
+    -- formatting & linting
+    use("jose-elias-alvarez/null-ls.nvim")
+    use("jayp0521/mason-null-ls.nvim")
+
+    if packer_bootstrap then
+        require("packer").sync()
+    end
+end)
