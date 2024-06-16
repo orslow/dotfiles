a509a7cace155be9658d39cde8b5b057cd80db3e
jueon <jueon@jueonui-MacBookAir.local>
2024-06-16 17:31:08 +0900

    From air

--- config/nvim/lua/plugins/init.lua
+++ config/nvim/lua/plugins/init.lua
@@ -1,6 +1,12 @@
 require('plugins.nvim-tree')
 require('plugins.colorscheme')
 require('plugins.telescope')
-require('plugins.vim-maximizer')
 require('plugins.lsp')
-require('plugins.vim-terraform')
+
+--- general keymap/configs
+
+vim.keymap.set("n", "<leader>z", ":MaximizerToggle<CR>")
+
+vim.g.terraform_fmt_on_save = 1
+
+vim.keymap.set('n', '<Leader>bl', ':BlameToggle<CR>', {})
--- config/nvim/lua/plugins/vim-terraform.lua
+++
@@ -1,3 +0,0 @@
--- require('vim-terraform').setup()
-
-vim.g.terraform_fmt_on_save = 1
